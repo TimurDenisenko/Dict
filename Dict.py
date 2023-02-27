aaa84374bcc6fd3a0b44a429853c361fb9be7451
User <User@DESKTOP-PICVUE1>
2023-02-28 01:21:02 +0200

    dict2

--- Dict.py
+++ Dict.py
@@ -1,14 +1,13 @@
 from kot_v_sapogah import *
-
-eurig1=loe("riigid_pealinnad.txt")
-eurig2=loe1("riigid_pealinnad.txt")
+eurig1,eurig2=loe("riigid_pealinnad.txt")
 while True:
     menu=input("""
 1-Vaatama Euroopa riigid 
 2-Otsige riiki või pealinna 
 3-Lisa riik ja pealinn
 4-Paranda riik või pealinn
-5-Harjutus
+5-Salvestama
+6-Harjutus
 """)
     if menu=="1":
         print(eurig1)
@@ -18,5 +17,10 @@ while True:
         eurig1=lisa(eurig1,eurig2)
     elif menu=="4":
         eurig1,eurig2=paranda(eurig1,eurig2)
+    elif menu=="5":
+        salv(eurig1,"riigid_pealinnad.txt")
+        print("Fail salvestatud!")
+    elif menu=="6":
+        trenn(eurig1)
     else:
         print("Kirjutage ainult need numbrid, mis teil on ") 
--- kot_v_sapogah.py
+++ kot_v_sapogah.py
@@ -1,26 +1,21 @@
 from os import * 
 from gtts import *
+from random import *
 import io
 
 def loe(file):
     x={} 
+    x1={}
     with io.open(file,"r",encoding="utf-8-sig") as f:
         for line in f:
             k,v=line.strip().split('-')
             x[k]=v
-    return x
-
-def loe1(file):
-    x={} 
-    with io.open(file,"r",encoding="utf-8-sig") as f:
-        for line in f:
-            k,v=line.strip().split('-')
-            x[v]=k
-    return x
+            x1[v]=k
+    return x,x1
 
 def heli(text:str,keel:str):
     obj=gTTS(text=text,lang=keel,slow=False).save("chinano.mp3") 
-    os.system("chinano.mp3")
+    system("chinano.mp3")
 
 def otsi(x:dict,x1:dict):
     sona=input("Kirjutage riik või linn ")
@@ -62,9 +57,11 @@ def lisa(x:dict,x1:dict):
 def paranda(x:dict,x1:dict): 
     sona=input("Kirjutage pealinn või riik, mida parandada ")
     while sona not in x and sona not in x1: 
-        sona=input("Kirjutage õige riik või pealinn ") 
+        sona=input("Kirjutage õige riik või pealinn ")
     if sona in x:
+        sona1=x.get(sona)
         x.pop(sona)
+        x1.pop(sona1)
         r=input("Kirjutage parandatud riigi ") 
         kus=input("Kas soovite parandada ka pealinn? (jah või ei) ").lower() 
         while kus not in ["jah","ei"]:
@@ -72,15 +69,55 @@ def paranda(x:dict,x1:dict):
         if kus=="jah":
             p=input("Kirjutage parandatud pealinn ")
     else:
+        sona1=x1.get(sona)
+        x.pop(sona1)
         x1.pop(sona)
         p=input("Kirjutage parandatud pealinn ") 
         kus=input("Kas soovite parandada ka riigi? (jah või ei) ").lower() 
         while kus not in ["jah","ei"]:
             kus=input("Kirjutage ainult jah või ei ").lower()
         if kus=="jah":
-            p=input("Kirjutage parandatud pealinn ") 
+            r=input("Kirjutage parandatud riik ") 
     x.update({r:p}) 
     x1.update({p:r})
     return x,x1
 
-def trenn(x:dict,x1:dict):
+def salv(x,fail):
+    xriik=list(x.keys())
+    xlinn=list(x.values()) 
+    y=[]
+    for i in range(len(x)):
+        y.append(xriik[i]+"-"+xlinn[i]+"\n")
+    with io.open(fail,"w",encoding="utf-8-sig") as f:
+        f.writelines(y)
+        
+def trenn(x:dict):
+    xriik=list(x.keys())
+    xlinn=list(x.values())
+    use=[]
+    a=0
+    n=input("Mitu korda me treenime ")
+    while n.isdigit()==False or int(n)>(len(x)*2):
+        n=input("Kirjutage õige arv ")
+    for i in range(int(n)):
+        ran=choice(choice([xriik,xlinn]))
+        while ran in use:
+            ran=choice(choice([xriik,xlinn]))
+        use.append(ran)
+        if ran in xlinn:
+            ind=xlinn.index(ran)
+            sona=input(f"Kirjutage pealinna {ran} riik ")
+            if sona==xriik[ind]:
+                print("Õige")
+                a+=1
+            else:
+                print("Vale")
+        else:
+            ind=xriik.index(ran)
+            sona=input(f"Kirjutage riigi {ran} pealinna ")
+            if sona==xlinn[ind]:
+                print("Õige")
+                a+=1
+            else:
+                print("Vale")
+    print(f"{a/int(n)*100}% vastused on õige ja {100-a/int(n)*100}% on vale")
